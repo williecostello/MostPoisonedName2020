ca0919b52d2159cb8a82c02e03b9ae095ba82c77
williecostello <williecostello@gmail.com>
2020-10-21 19:19:46 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+'''
+# Baby Name Popularity Explorer :baby: :telescope:
+
+An app to explore the popularity of baby names in the U.S. over time, from 1880 to 2020
+'''
+
+@st.cache(allow_output_mutation=True)
+def load_data(path):
+    df = pd.read_csv(path, index_col='year')
+    names = df.columns
+    return df, names
+
+df, names = load_data('data/babynamepcts_abrg.csv')
+
+selected_names = st.multiselect('Type to select baby names to explore', names)
+
+if len(selected_names) != 0:
+
+    chart_data = df[selected_names]
+
+    fig = px.line(
+        chart_data, 
+        labels={"year":"Year",  "value":"Percentage of year's total baby names", "variable":"Name"},
+    )
+
+    st.plotly_chart(fig)
+
+'''
+Visualizations based on data made available by the [U.S. Social Security Administration](https://catalog.data.gov/dataset/baby-names-from-social-security-card-applications-national-level-data)
+
+To see the full code and accompanying statistical analyses, see our [Github repo]()
+'''
---
+++ setup.sh
@@ -0,0 +1,29 @@
+# Create data directories
+mkdir -p data/raw_data
+mkdir -p data/proc_data
+
+echo 'Downloading data'
+
+# Download & unzip data
+curl https://www.ssa.gov/oact/babynames/names.zip -o data/raw_data/data.zip
+unzip data/raw_data/data.zip -d data/raw_data/
+
+echo 'Data downloaded'
+
+# Rename data files to remove 'yob' at the start of each filename
+for file in data/raw_data/*.txt
+do
+    mv "${file}" "${file/yob/}"
+done
+
+# Add appropriate year to each line in each data file
+for file in data/raw_data/*.txt
+do
+    year=$(basename $file .txt)
+    sed 's/^/'"$year"',/' "$file" > data/proc_data/"$year".csv
+done
+
+# Concatenate data files together into a single
+cat data/proc_data/*.csv > data/babynames.csv
+
+echo 'Files processed'
