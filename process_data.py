f858cdfb0e566ad0710a1b283a6ebc393be0c590
williecostello <williecostello@gmail.com>
2020-11-02 17:51:40 -0500

    Add two- and ten-year cumulative sum columns

--- process_data.py
+++ process_data.py
@@ -55,11 +55,11 @@ df_pctchg = df_pcts.pct_change()
 # Unpivot the percent change dataframe
 df_pctchg_melt = df_pctchg.reset_index().melt(id_vars='year', value_name='loss')
 
-# Calculate the 5-year percent change in yearly percentage
-df_pctchg_5 = df_pcts.pct_change(periods=5)
+# Calculate the 2-year percent change in yearly percentage
+df_pctchg_2 = df_pcts.pct_change(periods=2)
 
-# Unpivot the 5-year percent change dataframe
-df_pctchg_5_melt = df_pctchg_5.reset_index().melt(id_vars='year', value_name='lag_loss')
+# Unpivot the 2-year percent change dataframe
+df_pctchg_2_melt = df_pctchg_2.reset_index().melt(id_vars='year', value_name='loss_2')
 
 
 
@@ -72,16 +72,16 @@ df_a = df_count.copy()
 # Unpivot the percentages dataframe
 df_b = df_pcts.reset_index().melt(id_vars='year', value_name='percent')
 
-# Add columns for the 1-year and 5-year percent change
+# Add columns for the 1-year, 2-year, and 5-year percent change
 df_b['loss'] = df_pctchg_melt['loss']
-df_b['lag_loss'] = df_pctchg_5_melt['lag_loss']
+df_b['loss_2'] = df_pctchg_2_melt['loss_2']
 
 # Change the index of these dataframe to each row's name/year pair
 df_a.index = df_a['name'] + '_' + df_a['year'].astype(str)
 df_b.index = df_b['name'] + '_' + df_b['year'].astype(str)
 
 # Join the dataframes together
-df = df_a.join(df_b[['percent', 'loss', 'lag_loss']])
+df = df_a.join(df_b[['percent', 'loss', 'loss_2']])
 
 
 
@@ -105,6 +105,31 @@ for year in years:
     
 # Add cumsum column to master dataframe
 df['cumsum'] = cumsum_col
+
+
+
+# Add columns listing each name's cumulative sum value from two & ten years prior
+
+# Create a pivot table listing the cumulative sum of each name for each year
+df_cumsum_pivot = pd.pivot_table(df, values='cumsum', index='year', columns='name')
+
+# Change value to name's cumulative sum value from two / ten years prior
+df_cumsum_pivot_lag_2 = df_cumsum_pivot.shift(periods=2)
+df_cumsum_pivot_lag_10 = df_cumsum_pivot.shift(periods=10)
+
+# Unpivot the 2- / 10-years-prior cumulative sum dataframes
+df_cumsum_lag_2 = df_cumsum_pivot_lag_2.reset_index().melt(id_vars='year', value_name='cumsum_lag_2')
+df_cumsum_lag_10 = df_cumsum_pivot_lag_10.reset_index().melt(id_vars='year', value_name='cumsum_lag_10')
+
+# Combine these dataframes into a single dataframe
+df_c = df_cumsum_lag_2.copy()
+df_c['cumsum_lag_10'] = df_cumsum_lag_10['cumsum_lag_10']
+
+# Change the index of this dataframe to each row's name/year pair
+df_c.index = df_c['name'] + '_' + df_c['year'].astype(str)
+
+# Join new columns to master dataframe
+df = df.join(df_c[['cumsum_lag_2', 'cumsum_lag_10']])
 
 
 
